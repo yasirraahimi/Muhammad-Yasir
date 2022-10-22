b1be23321ed4b4558e5b162de071b9ab18bb0175
Muhammad Yasir <100397581+yasirraahimi@users.noreply.github.com>
2022-10-22 19:16:38 +0500

    Upload
    uploaded

--- responsive.js
+++ responsive.js
@@ -12,51 +12,7 @@ function toggleButton(){
 function featuredImg(x) {
   if (y.matches) { // If media query matches
     console.log("if clicked")
-   function allScroll (){
-    setTimeout(function(){imgFun();}, 1000);
-function imgFun(){
-    // console.log("Clicked..")
-    document.getElementById("img1").style.transform = "translateX(0px)"; 
-    document.getElementById("img2").style.transform = "translateX(0px)";  
-    document.getElementById("img3").style.transform = "translateX(0px)"; 
-    document.getElementById("img4").style.transform = "translateX(0px)"; 
-    document.getElementById("img5").style.transform = "translateX(0px)"; 
-    document.getElementById("img6").style.transform = "translateX(0px)"; 
-}
-    setTimeout(function(){imgFun1();}, 5000);
-function imgFun1(){
-    // console.log("Clicked..")
-    document.getElementById("img1").style.transform = "translateX(-425px)"; 
-    document.getElementById("img2").style.transform = "translateX(-425px)"; 
-    document.getElementById("img3").style.transform = "translateX(-425px)"; 
-    document.getElementById("img4").style.transform = "translateX(-425px)"; 
-    document.getElementById("img5").style.transform = "translateX(-425px)"; 
-    document.getElementById("img6").style.transform = "translateX(-425px)"; 
-}
-setTimeout(function(){imgFun2();}, 10000);
-function imgFun2(){
-    // console.log("Clicked..")
-    document.getElementById("img1").style.transform = "translateX(-850px)"; 
-    document.getElementById("img2").style.transform = "translateX(-850px)"; 
-    document.getElementById("img3").style.transform = "translateX(-850px)"; 
-    document.getElementById("img4").style.transform = "translateX(-850px)"; 
-    document.getElementById("img5").style.transform = "translateX(-850px)"; 
-    document.getElementById("img6").style.transform = "translateX(-850px)"; 
-}
-
-setTimeout(function(){imgFun3();}, 15000);
-function imgFun3(){
-    // console.log("Clicked..")
-    document.getElementById("img1").style.transform = "translateX(-1275px)"; 
-    document.getElementById("img2").style.transform = "translateX(-1275px)"; 
-    document.getElementById("img3").style.transform = "translateX(-1275px)"; 
-    document.getElementById("img4").style.transform = "translateX(-1275px)"; 
-    document.getElementById("img5").style.transform = "translateX(-1275px)"; 
-    document.getElementById("img6").style.transform = "translateX(-1275px)"; 
-}
-}
-setTimeout(function(){allScroll() , 1000})
-setInterval(function(){allScroll()} ,17000);
+ 
   } else {
    console.group("else")
    
